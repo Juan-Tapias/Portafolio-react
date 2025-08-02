5dd33e9a47c153ea1329f62a5e500b61faa1ca34
Juan-Tapias <jmartineztapias15@gmail.com>
2025-08-01 21:29:08 -0500

    feat: :sparkles: Se creo la seccion proyectos

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Header from './header.jsx';
 import Inicio from './inicio.jsx';
 import SobreMi from './sobre-mi.jsx'
 import Skills from './skills.jsx'
+import Proyectos from './proyecto.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')).render(
     <Inicio />
     <SobreMi />
     <Skills />
+    <Proyectos />
   </StrictMode>
 );
---
+++ src/proyecto.jsx
@@ -0,0 +1,18 @@
+import "./css/proyectos.css"
+
+function Proyectos (){
+    return(
+        <>
+        <section className="proyecto">
+            <div className="contenedor">
+                <img src="/img/bank.png" alt="Banco" />
+                <img src="/img/bodas.png" alt="Banco" />
+                <img src="/img/ecma.png" alt="Banco" />
+                <img src="/img/flashcard.png" alt="Banco" />
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Proyectos
