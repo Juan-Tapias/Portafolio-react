2c0a02635fbf86b8bb4c29e8f06dbe4485a3066b
Juan-Tapias <jmartineztapias15@gmail.com>
2025-08-14 22:52:12 -0500

    refactor: :zap: Se renderizo la seccion de soft-skills

--- src/soft-skills.jsx
+++ src/soft-skills.jsx
@@ -1,4 +1,15 @@
 import './css/soft-skills.css'
+
+const skills = [
+    {title: 'Trabajo en equipo', description: 'Capacidad para colaborar y contribuir en grupos diversos.'},
+    {title: 'Comunicacion efectiva', description: 'Habilidad para expresar ideas de manera clara y persuasiva.'},
+    {title: 'Resolucion de problemas', description: 'Enfoque analítico para abordar desafíos complejos.'},
+    {title: 'Liderazgo', description: 'Habilidad para inspirar y guiar a otros hacia objetivos comunes.'},
+    {title: 'Adaptabilidad', description: 'Flexibilidad para ajustarse a nuevas situaciones y tecnologías.'},
+    {title: 'Creatividad', description: 'Capacidad para generar ideas innovadoras y soluciones originales.'},
+    {title: 'Gestión del tiempo', description: 'Habilidad para priorizar tareas y cumplir plazos eficientemente.'},
+    {title: 'Empatia', description: 'Capacidad para entender y considerar las perspectivas de otros.'},
+]
 function Soft() {
     return (
        <>
@@ -8,30 +19,12 @@ function Soft() {
             </div>
             <div className="skills-soft">
                 <div className="container-soft" id="original">
-                    <div className="soft"> 
-                        <p>Trabajo en equipo</p>
-                    </div>
-                    <div className="soft">
-                        <p>Comunicacion efectiva</p>
-                    </div>
-                    <div className="soft">
-                        <p>Resolucion de problemas</p>
-                    </div>
-                    <div className="soft"> 
-                        <p>Liderazgo</p>
-                    </div>
-                    <div className="soft"> 
-                        <p>Adaptabilidad</p>
-                    </div>
-                    <div className="soft"> 
-                        <p>Creatividad</p>
-                    </div>
-                    <div className="soft"> 
-                        <p>Gestión del tiempo</p>
-                    </div>
-                    <div className="soft">
-                        <p>Empatia</p>
-                    </div>
+                    {skills.map((skill, index) => (
+                        <div className="soft" key={index}> 
+                        <p className='title'>{skill.title}</p>
+                        <p className='description'>{skill.description}</p>
+                    </div> 
+                    ))}
                 </div>
             </div>
         </section>
